763c15fbdebfbc41d821ff7a8014d0909bad3eb0
Vedant Bhalerao <vedantbhalerao343@gmail.com>
2024-02-01 22:04:38 +0530

    Authentication added

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
-import Image from "next/image";
-
+// import { Button } from "@/components/ui/button";
+// import Image from "next/image";
+import { UserButton } from "@clerk/nextjs";
 export default function Home() {
-  return <p className="text-6xl text-green-700">Hello guyzz hello world</p>;
+  return (
+    <div className="h-screen">
+      <UserButton afterSignOutUrl="/" />
+    </div>
+  );
 }
